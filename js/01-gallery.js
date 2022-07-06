85999682af9fd3d2ea1c3342d03038a1807b1537
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-07-06 15:30:27 +0300

    first task done

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryWrap = document.querySelector('.gallery');
+
+// console.log(galleryItems);
+
+
+function createGallery() {
+	const markup = galleryItems.map(item => `
+		<div class="gallery__item">
+	        <a class="gallery__link" href="${item.original}">
+		    <img
+		      class="gallery__image"
+		      src="${item.preview}"
+		      data-source="${item.original}"
+		      alt="${item.description}"
+		    />
+	        </a>
+		</div>
+	`).join('');
+
+	galleryWrap.insertAdjacentHTML('beforeend', markup);
+}
+
+createGallery();
+
+galleryWrap.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+	e.preventDefault();
+
+	if (e.target.nodeName !== 'IMG') {
+		return;
+	}
+
+	const instance = basicLightbox.create(`
+      <img src="${e.target.dataset.source}" width="800" height="600">
+	`);
+
+	instance.show();
+
+	document.addEventListener('keydown', onEscKeyPress);
+
+	function onEscKeyPress(e) {
+		if (e.code === 'Escape') {
+			instance.close();
+		}
+	}
+}
