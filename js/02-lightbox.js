69cefa688a3f4c50c096db7830c9dcf27a8ddc92
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-07-07 12:38:24 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,6 @@
 import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
-
 const galleryList = document.querySelector('.gallery');
 
 const renderGallery = function () {
