6c52279cbf5d62a2e8655b8e4bb329cbee7f83aa
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-07-07 10:37:14 +0300

    task 2 done

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
-A
-js/02-lightbox.js,8/b/8b1c43b2bb1cc0d9d76dfcfd321c4f35ccc17c35
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const renderGallery = function () {
+	const markup = galleryItems.map(item => `
+		<a class="gallery__item" href="${item.original}">
+			<img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+		</a>
+	`).join('');
+
+	galleryList.insertAdjacentHTML('beforeend', markup);
+}
+
+renderGallery();
+
+let lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionPosition: 'bottom',
+	captionDelay: 250,
+});
